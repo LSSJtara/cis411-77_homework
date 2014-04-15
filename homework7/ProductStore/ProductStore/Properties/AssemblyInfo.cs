62a9e9ba835bfa36ed2241db134c85fadbc22d6a
LSSJtara <t0nanc01@louisville.edu>
2014-04-15 19:00:50 -0400

    created Product Store project

--- homework7/ProductStore/ProductStore/Properties/AssemblyInfo.cs
+++ homework7/ProductStore/ProductStore/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e26275f5-a14c-4032-958a-8eaa7642cb6a")]
+[assembly: Guid("726a42e6-4530-4354-a6ae-a492e0f400d4")]
 
 // Version information for an assembly consists of the following four values:
 //
