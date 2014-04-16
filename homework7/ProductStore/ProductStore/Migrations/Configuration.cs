950de0396888a02edc5a0bfbfd0ddbd845e185af
LSSJtara <t0nanc01@louisville.edu>
2014-04-16 15:43:38 -0400

    Added dynamic UI for Admins that utilizes Knockout.js

---
+++ homework7/ProductStore/ProductStore/Migrations/Configuration.cs
@@ -0,0 +1,41 @@
+namespace ProductStore.Migrations
+{
+    using ProductStore.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProductStore.Models.OrdersContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OrdersContext context)
+        {
+            var products = new List<Product>()            
+            {
+                new Product() { Name = "Tomato Soup", Price = 1.39M, ActualCost = .99M },
+                new Product() { Name = "Hammer", Price = 16.99M, ActualCost = 10 },
+                new Product() { Name = "Yo yo", Price = 6.99M, ActualCost = 2.05M }
+            };
+
+            products.ForEach(p => context.Products.Add(p));
+            context.SaveChanges();
+
+            var order = new Order() { Customer = "Bob" };
+            var od = new List<OrderDetail>()
+            {
+                new OrderDetail() { Product = products[0], Quantity = 2, Order = order},
+                new OrderDetail() { Product = products[1], Quantity = 4, Order = order }
+            };
+            context.Orders.Add(order);
+            od.ForEach(o => context.OrderDetails.Add(o));
+
+            context.SaveChanges();
+        }
+    }
+}
--- homework7/ProductStore/ProductStore/Models/OrdersContextInitializer.cs
+++ homework7/ProductStore/ProductStore/Models/OrdersContextInitializer.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace ProductStore.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Data.Entity;
-
     public class OrdersContextInitializer : DropCreateDatabaseIfModelChanges<OrdersContext>
     {
         protected override void Seed(OrdersContext context)
